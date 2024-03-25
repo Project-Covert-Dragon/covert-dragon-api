ea42e0ffa106d75959f86181e120ced3e043ec3f
Justin Lenhart <159576013+lenhart105@users.noreply.github.com>
2024-03-25 17:07:43 -0400

    ef-core

---
+++ Covert.Dragon.Data/StoreContext.cs
@@ -0,0 +1,22 @@
+﻿using Covert.Dragon.Domain.Orders;
+using Covert.Dragon.Domain.Catalog;
+using Microsoft.EntityFrameworkCore; 
+
+namespace Covert.Dragon.Data 
+{ 
+    public class StoreContext: DbContext 
+    { 
+        public StoreContext( DbContextOptions< StoreContext> options)
+           : base( options) 
+{}
+public DbSet<Item> Items { get; set; }
+
+public DbSet<Order> Orders {get ; set;}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            DbInitializer.Initialize(builder);
+
+        }
+    }
+}
---
+++ Covert.Dragon.Domain/Catalog/Item.cs
@@ -0,0 +1,43 @@
+
+using System;
+using System.Collections.Generic; // Import List type
+
+namespace Covert.Dragon.Api.Domain.Catalog
+{
+    public class Item
+    {
+        // Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+
+        // Constructor
+        public Item(string name, string description, string brand, decimal price)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentNullException(nameof(description));
+            }
+            if (string.IsNullOrEmpty(brand))
+            {
+                throw new ArgumentNullException(nameof(brand));
+            }
+            if (price < 0.00m)
+            {
+                throw new ArgumentException("Price must be greater than zero.");
+            }
+
+            Name = name;
+            Description = description;
+            Brand = brand;
+            Price = price;
+        }
+    }
+}
