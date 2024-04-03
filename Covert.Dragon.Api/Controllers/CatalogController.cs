f5fdf1c90351064d1eec2023516122840c602fe3
Casey Curran <caseycurran5@gmail.com>
2024-04-02 21:40:17 -0400

    finished video 2

--- Covert.Dragon.Api/Controllers/CatalogController.cs
+++ Covert.Dragon.Api/Controllers/CatalogController.cs
@@ -18,5 +18,47 @@ namespace Covert.Dragon.Api.Controllers{
 
             return Ok(items);
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id){
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+            item.Id =id;
+            return Ok(item);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("catalog/42", item);
+        }
+
+        [HttpPost("{id}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating){
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+
+        /*
+        {
+            "stars":5,
+            "userName": "John Smith",
+            "review": "Great!"
+        }
+        */
+
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id){
+            return NoContent();
+        }
     }
 }
--- Covert.Dragon.Domain/Catalog/Item.cs
+++ Covert.Dragon.Domain/Catalog/Item.cs
@@ -7,7 +7,7 @@ namespace Covert.Dragon.Domain.Catalog{
         public string? Description {get; set;}
         public string? Brand {get; set;}
         public decimal Price {get; set;}
-        public List<Rating> Ratings {get; set;}
+        public List<Rating>? Ratings {get; set;}
 
         public void AddRating(Rating rating){
             this.Ratings.Add(rating);
@@ -35,6 +35,7 @@ namespace Covert.Dragon.Domain.Catalog{
             this.Description = description;
             this.Brand = brand;
             this.Price = price;
+            this.Ratings = new List<Rating>();
         }
     }
 }
