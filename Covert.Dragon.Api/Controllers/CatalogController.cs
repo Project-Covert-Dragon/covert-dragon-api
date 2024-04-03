a336a09eebe52a73a837f2f1e2372bd3778c9ee5
Casey Curran <caseycurran5@gmail.com>
2024-04-02 21:00:35 -0400

    finishing another part

---
+++ Covert.Dragon.Api/Controllers/CatalogController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Covert.Dragon.Domain.Catalog;
+
+namespace Covert.Dragon.Api.Controllers{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CatalogController: ControllerBase {
+        [HttpGet]
+        public IActionResult GetItems(){
+            return Ok("Hello world!");
+        }
+    }
+}
