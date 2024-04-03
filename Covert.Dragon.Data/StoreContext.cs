4c9847f6ae013497d57c5968229cab514d4e8ab7
Casey Curran <caseycurran5@gmail.com>
2024-04-03 18:17:23 -0400

    finished page 23

--- Covert.Dragon.Data/StoreContext.cs
+++ Covert.Dragon.Data/StoreContext.cs
@@ -8,5 +8,11 @@ namespace Covert.Dragon.Data
         public StoreContext(DbContextOptions<StoreContext> options): base(options)
         {}
         public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            DbInitializer.Initialize(builder);
+        }
     }
 }
