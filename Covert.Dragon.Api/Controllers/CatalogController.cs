1bc3fc8c2485c3e66338f9b4d23c3d4c9c318864
Casey Curran <caseycurran5@gmail.com>
2024-04-03 18:28:33 -0400

    finished page 25

--- Covert.Dragon.Api/Controllers/CatalogController.cs
+++ Covert.Dragon.Api/Controllers/CatalogController.cs
@@ -53,7 +53,9 @@ namespace Covert.Dragon.Api.Controllers{
         [HttpPost]
         public IActionResult Post(Item item)
         {
-            return Created("catalog/42", item);
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
         }
 
         [HttpPost("{id}/ratings")]
